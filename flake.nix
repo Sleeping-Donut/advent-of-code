c040babf0575b5e1e5de56e5fdbfe1f4a6e619c4
Sleeping-Donut <nathandavis1999@gmail.com>
2025-01-07 23:22:07 +0000

    add dev shells for rust and python

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+	description = "Flake for advent of code dev shells";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }: let
+		allSystems = [
+			"aarch64-darwin"
+		];
+		forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+			pkgs = import nixpkgs { inherit system; };
+		});
+
+	in {
+		devShells = forAllSystems ({ pkgs }: {
+			python = pkgs.mkShell {
+				packages = with pkgs; [ python3Minimal uv ];
+			};
+			rust = pkgs.mkShell {
+				nativeBuildInputs = with pkgs; [
+					rustc cargo gcc rustfmt clippy
+				];
+			};
+		});
+	};
+}
