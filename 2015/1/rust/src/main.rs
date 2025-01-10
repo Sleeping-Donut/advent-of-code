3e75b34767c15c8c414d184edf9aaa8927a847b1
Sleeping-Donut <nathandavis1999@gmail.com>
2025-01-10 02:28:14 +0000

    solution 2015-01 rust part 2

--- 2015/1/rust/src/main.rs
+++ 2015/1/rust/src/main.rs
@@ -11,13 +11,20 @@ fn main() {
     let file_reader = read_lines(file_path).expect("Could not read file");
 
     let mut floor = 0;
+    let mut char_counter = 0;
+    let mut basement_entered = false;
 
     for line in file_reader {
         for char in line.unwrap_or_default().chars() {
+            char_counter += 1;
             match char {
                 '(' => floor += 1,
                 ')' => floor -= 1,
                 _ => (),
+            }
+            if !basement_entered && floor == -1 {
+                basement_entered = true;
+                println!("basement entered at pos {}", char_counter)
             }
         }
     }
