756fc25853d6182071dd8dc50f2376216b89e983
Sleeping-Donut <nathandavis1999@gmail.com>
2025-01-13 20:49:24 +0000

    cleanup solution 2015-02 rust

--- 2015/2/rust/src/main.rs
+++ 2015/2/rust/src/main.rs
@@ -1,8 +1,7 @@
 use std::{
     env::args,
     fs::File,
-    io::{self, BufRead, BufReader, Lines},
-    path::Path,
+    io::{self, BufRead, BufReader},
 };
 
 fn main() {
@@ -11,11 +10,26 @@ fn main() {
         .nth(1)
         .expect("Need to provide an argument for the input file");
 
-    let file = read_lines(file_path).expect("The input file should be there");
+    let file = File::open(file_path).expect("The input file must exist");
+    let file_reader = BufReader::new(file);
 
-    let mut total_paper = 0;
-    let mut total_ribbon = 0;
-    for line_reader in file {
+    let christmas_order = calculate_order_quantity(file_reader.lines());
+    println!("total paper: {}", christmas_order.paper);
+    println!("total ribbon: {}", christmas_order.ribbon);
+}
+
+struct ChristmasOrder {
+    paper: usize,
+    ribbon: usize,
+}
+
+fn calculate_order_quantity<I>(lines: I) -> ChristmasOrder
+where
+    I: Iterator<Item = Result<String, io::Error>>,
+{
+    let mut total_paper: usize = 0;
+    let mut total_ribbon: usize = 0;
+    for line_reader in lines {
         let line = line_reader.expect("Oh no, error reading line!");
 
         // surface area =
@@ -29,53 +43,47 @@ fn main() {
             .expect("Could not find 2 smallest in dimensions");
 
         // slack area is the product of the 2 smallest dimensions
-        let slack_area: i32 = smallest.iter().product();
+        let slack_area: usize = smallest.iter().product();
 
         // ribbon wrap = smallest perimiter of any one face
         // ribbon bow = cubic volume of the dimensions
         // ribbon length = wrap + bow
-        let ribbon_length = (smallest.iter().sum::<i32>() * 2) + (width * length * height);
+        let ribbon_length = (smallest.iter().sum::<usize>() * 2) + (width * length * height);
 
         total_paper += surface_area + slack_area;
         total_ribbon += ribbon_length;
     }
-    println!("total paper: {}", total_paper);
-    println!("total ribbon: {}", total_ribbon);
+    ChristmasOrder {
+        paper: total_paper,
+        ribbon: total_ribbon,
+    }
 }
 
-fn read_lines<P>(file_path: P) -> io::Result<Lines<BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(file_path)?;
-    Ok(BufReader::new(file).lines())
-}
-
-fn parse_dimensions(text_dimensions: String) -> Result<(i32, i32, i32), ()> {
+fn parse_dimensions(text_dimensions: String) -> Result<(usize, usize, usize), ()> {
     let mut dimensions = text_dimensions.split("x");
     let length = dimensions
         .next()
         .expect("Could not find first dimension")
         .trim()
-        .parse::<i32>()
+        .parse::<usize>()
         .expect("Could not parse first dimension");
     let width = dimensions
         .next()
         .expect("Could not find second dimension")
         .trim()
-        .parse::<i32>()
+        .parse::<usize>()
         .expect("Could not parse second dimension");
     let height = dimensions
         .next()
         .expect("Could not find second dimension")
         .trim()
-        .parse::<i32>()
+        .parse::<usize>()
         .expect("Could not parse second dimension");
     Ok((length, width, height))
 }
 
-fn n_smallest(lengths: &[i32], n: u32) -> Result<Vec<i32>, ()> {
-    let mut smallest_numbers: Vec<i32> = vec![];
+fn n_smallest(lengths: &[usize], n: u32) -> Result<Vec<usize>, ()> {
+    let mut smallest_numbers: Vec<usize> = vec![];
     match lengths.iter().min() {
         Some(val) => smallest_numbers.push(*val),
         None => return Err(()),
@@ -89,7 +97,7 @@ fn n_smallest(lengths: &[i32], n: u32) -> Result<Vec<i32>, ()> {
             .iter()
             .position(|val| val == smallest)
             .expect("Value should exist as it was just found");
-        let slice: Vec<&i32> = lengths[..position]
+        let slice: Vec<&usize> = lengths[..position]
             .iter()
             .chain(lengths[position + 1..].iter())
             .collect();
