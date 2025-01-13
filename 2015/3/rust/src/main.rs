594b9b9d510542f88cfeca149d41d98bfd06e323
Sleeping-Donut <nathandavis1999@gmail.com>
2025-01-13 18:53:03 +0000

    solution 2015-03 rust part 2

--- 2015/3/rust/src/main.rs
+++ 2015/3/rust/src/main.rs
@@ -2,8 +2,7 @@ use std::{
     collections::HashSet,
     env::args,
     fs::File,
-    io::{self, BufRead, BufReader, Lines},
-    path::Path,
+    io::{self, BufRead, BufReader, Read, Seek},
 };
 
 fn main() {
@@ -12,35 +11,60 @@ fn main() {
         .nth(1)
         .expect("Need to provide an argument for the input file");
 
-    let file = read_lines(file_path).expect("The input file should be there");
+    let file = File::open(file_path).expect("File must exist");
+    let mut file_reader = BufReader::new(file);
 
+    let single_santa_visited = santas_visits(file_reader.by_ref().lines(), 1);
+    println!("visited: {}", single_santa_visited);
+
+    file_reader
+        .get_mut()
+        .seek(io::SeekFrom::Start(0))
+        .expect("Cannot seek to beginning");
+    let with_robo_visits = santas_visits(file_reader.by_ref().lines(), 2);
+    println!("visited: {}", with_robo_visits);
+}
+
+#[derive(Default, Clone)]
+struct Position {
+    x: isize,
+    y: isize,
+}
+
+fn santas_visits<I>(lines: I, n: usize) -> usize
+where
+    I: Iterator<Item = Result<String, io::Error>>,
+{
     let mut houses = HashSet::new();
-    let mut pos_x: isize = 0;
-    let mut pos_y: isize = 0;
+    let mut santas_pos: Vec<Position> = vec![Position::default(); n];
     let mut visited: usize = 0;
+    let mut which_santa = 0;
 
-    for line in file {
+    // Starting location counts as visited
+    houses.insert("0-0".to_string()); // starting pos
+    visited += 1;
+
+    for line in lines {
         for char in line.unwrap_or_default().chars() {
-            match char {
-                '<' => pos_x -= 1,
-                '>' => pos_x += 1,
-                'v' => pos_y -= 1,
-                '^' => pos_y += 1,
-                _ => (),
-            }
+            let (xd, yd) = match char {
+                '<' => (-1, 0),
+                '>' => (1, 0),
+                'v' => (0, -1),
+                '^' => (0, 1),
+                _ => (0, 0),
+            };
 
-            if houses.insert(format!("{}-{}", pos_x, pos_y)) {
+            santas_pos[which_santa].x += xd;
+            santas_pos[which_santa].y += yd;
+            if houses.insert(format!(
+                "{}-{}",
+                santas_pos[which_santa].x, santas_pos[which_santa].y
+            )) {
                 visited += 1;
             }
+            which_santa = (which_santa + 1) % n;
         }
     }
-    println!("visited: {}", visited)
+    visited
 }
 
-fn read_lines<P>(file_path: P) -> io::Result<Lines<BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(file_path)?;
-    Ok(BufReader::new(file).lines())
-}
