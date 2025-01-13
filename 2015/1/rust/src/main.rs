0c7ea12d1b542cfb00d5b850c7739216b27e04e9
Sleeping-Donut <nathandavis1999@gmail.com>
2025-01-13 20:23:31 +0000

    cleanup solution 2015-01 rust

--- 2015/1/rust/src/main.rs
+++ 2015/1/rust/src/main.rs
@@ -1,20 +1,32 @@
 use std::{
     env::args,
     fs::File,
-    io::{self, BufRead, BufReader, Lines},
-    path::Path,
+    io::{self, BufRead, BufReader},
 };
 
 fn main() {
     let file_path = args().nth(1).expect("No filepath provided");
 
-    let file_reader = read_lines(file_path).expect("Could not read file");
+    let file = File::open(file_path).expect("File must exist");
+    let file_reader = BufReader::new(file);
 
-    let mut floor = 0;
+    let (floor, basement_position) = santa_floors(file_reader.lines());
+
+    println!("floor {}", floor);
+    if let Some(position) = basement_position {
+        println!("basement entered at pos {}", position)
+    }
+}
+
+fn santa_floors<I>(lines: I) -> (isize, Option<usize>)
+where
+    I: Iterator<Item = Result<String, io::Error>>,
+{
+    let mut floor: isize = 0;
     let mut char_counter = 0;
-    let mut basement_entered = false;
+    let mut basement_entered = None;
 
-    for line in file_reader {
+    for line in lines {
         for char in line.unwrap_or_default().chars() {
             char_counter += 1;
             match char {
@@ -22,19 +34,10 @@ fn main() {
                 ')' => floor -= 1,
                 _ => (),
             }
-            if !basement_entered && floor == -1 {
-                basement_entered = true;
-                println!("basement entered at pos {}", char_counter)
+            if basement_entered.is_none() && floor == -1 {
+                basement_entered = Some(char_counter);
             }
         }
     }
-    println!("floor {}", floor)
-}
-
-fn read_lines<P>(file_path: P) -> io::Result<Lines<BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(file_path)?;
-    Ok(BufReader::new(file).lines())
+    (floor, basement_entered)
 }
